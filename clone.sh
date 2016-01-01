719e868c12a4c1037b77dac30592a2de1c26bb2c
Reuben Hawkins <reubenhwk@gmail.com>
2016-01-01 03:51:45 -0800

    boot properly on USB disk

--- clone.sh
+++ clone.sh
@@ -1,25 +1,53 @@
 #!/usr/bin/env bash
 
-DEV=/dev/sdd
-FSTYPE=ext4
-PART=${DEV}1
-TMP=$(mktemp -d)
-MNT=$TMP/mnt
-
 die () {
-	echo "$@"
+	echo "error: $@, exiting"
 	exit 1
 }
 
-prepare_device () {
-	(echo o; echo n; echo p; echo 1; echo; echo; echo a; echo w) | fdisk $DEV
-	partprobe
-	mkfs.$FSTYPE -F -F $PART
+DEV=$1
+
+DEVICE_INFO=$(fdisk -l $DEV | head -n 1)
+if [ "$DEVICE_INFO" = "" ] ; then
+	die "failed to get disk info on $DEV"
+fi
+
+echo -n "Use $DEVICE_INFO? (y/n) [n]: "
+read yesno
+if [ "$yesno" != "y" ] ; then
+	exit
+fi
+
+FSTYPE=ext4
+PART=${DEV}1
+TMNT=
+MNT=
+
+umount_all () {
+	sleep 2
+	for p in $DEV* ; do
+		if [ "$p" != "$DEV" ] ; then
+			umount $p
+		fi
+	done
 }
 
+# prepare_device () {
+# 	umount_all
+# 	(echo o; echo n; echo p; echo 1; echo; echo; echo a; echo w) | fdisk $DEV
+# 	partprobe
+# 	mkfs.$FSTYPE -F -F $PART
+# 	umount_all
+# }
+
 mount_device () {
-	mkdir -p $MNT || die "unable to mkdir $MNT"
-	mount $PART $MNT || die "unable to mount $PART on $MNT"
+	MNT=$(cat /proc/mounts | grep /dev/sde1 | cut -d\  -f2)
+	if (( $? == 0 )) ; then
+		TMNT=$(mktemp -d)
+		MNT=$TMNT
+		mkdir -p $MNT || die "unable to mkdir $MNT"
+		mount $PART $MNT || die "unable to mount $PART on $MNT"
+	fi
 }
 
 clone_fs () {
@@ -32,12 +60,13 @@ do_grub () {
 		mount --bind /$d $MNT/$d || die "failed to mount --bind /$d $MNT/$d"
 	done
 
-	grub-install $DEV || die "grub-install $DEV failed"
 
 	sed -i 's/GRUB_TIMEOUT=.*$/GRUB_TIMEOUT=30/g' /etc/default/grub
+	echo GRUB_DISABLE_OS_PROBER=true >> /etc/default/grub
 
 cat << EOF > $MNT/tmp/grub.sh
 #!/usr/bin/env bash
+grub-install $DEV
 update-grub
 EOF
 	chmod +x $MNT/tmp/grub.sh
@@ -62,15 +91,15 @@ cat << EOF > $MNT/etc/fstab
 # that works even if disks are added and removed. See fstab(5).
 #
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
-# / was on /dev/sda1 during installation
-UUID=$UUID /               $FSTYPE    errors=remount-ro 0       1
+UUID=$UUID / auto    errors=remount-ro 0       1
 EOF
 
 }
 
 cleanup () {
-	umount $MNT
-	rm -rf $TMP
+	if [ "$TMNT" != "" ] ; then
+		umount $TMNT && rmdir $TMNT
+	fi
 }
 
 main () {
@@ -83,3 +112,4 @@ main () {
 }
 
 main
+
