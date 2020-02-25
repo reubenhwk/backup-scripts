1156299d38a110a5bf7870ab629e9c31b8cfeadf
Reuben Hawkins <reubenhwk@gmail.com>
2020-02-25 07:42:33 -0800

    write stuff to .backup in walk-fs script

--- walk-fs.py
+++ walk-fs.py
@@ -2,38 +2,50 @@
 
 import os
 import hashlib
+import tempfile
 
 hashfunc = hashlib.sha224
 
 total_size = 0
 
+def xprint(fd, key=None, value=None):
+    if key and value:
+        os.write(fd, "{} {}\n".format(key, value).encode('utf-8'))
+    else:
+        os.write(fd, "\n".encode('utf-8'))
+
 def directory_handler(path):
     global total_size
     for dirent in os.listdir(path):
-        try:
-            fullpath = "{}/{}".format(path, dirent)
-            stat = os.lstat(fullpath)
-            if os.path.isdir(fullpath):
-                directory_handler(fullpath)
-            print("name", dirent)
-            print("mtime_ns", stat.st_mtime_ns)
-            print("mode", stat.st_mode)
-            print("uid", stat.st_uid)
-            print("gid", stat.st_gid)
-            print("size", stat.st_size)
-            if os.path.isfile(fullpath):
-                xhash = hashfunc()
-                with open(fullpath, 'rb') as f:
-                    data = f.read(64*1024)
-                    while len(data) > 0:
-                        xhash.update(data)
-                        data = f.read(64*1024)
-                digest = xhash.hexdigest()
-                print(xhash.name, digest)
-            print()
-            total_size += stat.st_size
-        except:
-            print("--> error", fullpath)
+        fullpath = "{}/{}".format(path, dirent)
+        if os.path.isdir(fullpath):
+            if dirent == ".backup":
+                continue
+            directory_handler(fullpath)
+        stat = os.lstat(fullpath)
 
+        fd, name = tempfile.mkstemp(dir=".backup")
+
+        xprint(fd, "name", dirent)
+        xprint(fd, "mtime_ns", stat.st_mtime_ns)
+        xprint(fd, "mode", stat.st_mode)
+        xprint(fd, "uid", stat.st_uid)
+        xprint(fd, "gid", stat.st_gid)
+        xprint(fd, "size", stat.st_size)
+
+        if os.path.isfile(fullpath):
+            xhash = hashfunc()
+            with open(fullpath, 'rb') as f:
+                data = f.read(64*1024)
+                while len(data) > 0:
+                    xhash.update(data)
+                    data = f.read(64*1024)
+            digest = xhash.hexdigest()
+            xprint(fd, xhash.name, digest)
+        xprint(fd)
+        os.close(fd)
+        total_size += stat.st_size
+
+os.makedirs(".backup", exist_ok=True)
 directory_handler(".")
 print("total_size", total_size)
