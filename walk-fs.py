68412f66a86281e1df041a1b8716a1e114c9139d
Reuben Hawkins <reubenhwk@gmail.com>
2020-03-08 13:39:46 -0700

    add file handler, output json

--- walk-fs.py
+++ walk-fs.py
@@ -3,16 +3,14 @@
 import os
 import hashlib
 import tempfile
+import json
 
 hashfunc = hashlib.sha224
 
 total_size = 0
 
-def xprint(fd, key=None, value=None):
-    if key and value:
-        os.write(fd, "{} {}\n".format(key, value).encode('utf-8'))
-    else:
-        os.write(fd, "\n".encode('utf-8'))
+def xprint(fd, inodes):
+    os.write(fd, json.dumps(inodes, indent=4, sort_keys=True).encode('utf-8'))
 
 def hash_file(name):
     xhash = hashfunc()
@@ -23,35 +21,45 @@ def hash_file(name):
             data = f.read(64*1024)
     return xhash.hexdigest(), xhash.name
 
+def file_handler(path):
+    digest, hash_name = hash_file(path)
+    return digest, hash_name
+
 def directory_handler(path):
     global total_size
-    fd, tmpfile_name = tempfile.mkstemp(dir=".backup")
-
+    inodes = list()
     for dirent in os.listdir(path):
+        inode = dict()
         fullpath = "{}/{}".format(path, dirent)
         if os.path.isdir(fullpath):
             if dirent == ".backup":
                 continue
-            directory_handler(fullpath)
+            digest, hash_name = directory_handler(fullpath)
+            inode[hash_name] = digest
+        elif os.path.isfile(fullpath):
+            digest, hash_name = file_handler(fullpath)
+            inode[hash_name] = digest
+
         stat = os.lstat(fullpath)
 
-        xprint(fd, "name", dirent)
-        xprint(fd, "mtime_ns", stat.st_mtime_ns)
-        xprint(fd, "mode", stat.st_mode)
-        xprint(fd, "uid", stat.st_uid)
-        xprint(fd, "gid", stat.st_gid)
-        xprint(fd, "size", stat.st_size)
+        inode["name"] =  dirent
+        inode["mtime_ns"] = stat.st_mtime_ns
+        inode["mode"] = stat.st_mode
+        inode["uid"] = stat.st_uid
+        inode["gid"] = stat.st_gid
+        inode["size"] = stat.st_size
+        inodes.append(inode)
 
-        if os.path.isfile(fullpath):
-            digest, hash_name = hash_file(fullpath)
-            xprint(fd, hash_name, digest)
-        xprint(fd)
         total_size += stat.st_size
+
+    fd, tmpfile_name = tempfile.mkstemp(dir=".backup")
+    xprint(fd, {"inodes" : inodes})
     os.close(fd)
+
     digest, hash_name = hash_file(tmpfile_name)
     os.rename(tmpfile_name, ".backup/{}".format(digest))
-    return digest
+    return digest, hash_name
 
 os.makedirs(".backup", exist_ok=True)
-print(".", directory_handler("."))
+print(".", directory_handler(".")[0])
 print("total_size", total_size)
