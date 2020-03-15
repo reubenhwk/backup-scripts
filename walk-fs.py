fd261524f6025d8ea174fc6fd60d41f49b24a6ed
Reuben Hawkins <reubenhwk@gmail.com>
2020-03-15 08:35:47 -0700

    make more functions

--- walk-fs.py
+++ walk-fs.py
@@ -9,6 +9,8 @@ import time
 hashfunc = hashlib.sha224
 
 total_size = 0
+os.makedirs(".backup/blobs", exist_ok=True)
+os.makedirs(".backup/tmp", exist_ok=True)
 
 def xprint(fd, inodes):
     os.write(fd, json.dumps(inodes, indent=4, sort_keys=True).encode('utf-8'))
@@ -60,31 +62,30 @@ def directory_handler(path):
     os.rename(tmpfile_name, ".backup/blobs/{}".format(digest))
     return digest, hash_name
 
-os.makedirs(".backup/blobs", exist_ok=True)
-os.makedirs(".backup/tmp", exist_ok=True)
+def make_backup():
+    # Make a head pointer stored in .backup/head (which is actually a symlink)
+    root_blob, hash_name = directory_handler(".")
+    fd, tmpfile_name = tempfile.mkstemp(dir=".backup/tmp")
+    head = dict()
+    head['date'] = time.time()
+    head['root'] = {hash_name : root_blob}
+    xprint(fd, head)
+    os.close(fd)
+    digest, hash_name = hash_file(tmpfile_name)
+    head_filename = ".backup/blobs/{}".format(digest)
+    os.rename(tmpfile_name, head_filename)
+    try:
+        os.remove(".backup/head.prev")
+    except:
+        pass
+    try:
+        os.rename(".backup/head", ".backup/head.prev")
+    except:
+        pass
+    os.symlink("blobs/{}".format(digest), ".backup/head")
 
-# Make a head pointer stored in .backup/head (which is actually a symlink)
-root_blob, hash_name = directory_handler(".")
-fd, tmpfile_name = tempfile.mkstemp(dir=".backup/tmp")
-head = dict()
-head['date'] = time.time()
-head['root'] = {hash_name : root_blob}
-xprint(fd, head)
-os.close(fd)
-digest, hash_name = hash_file(tmpfile_name)
-head_filename = ".backup/blobs/{}".format(digest)
-os.rename(tmpfile_name, head_filename)
-try:
-    os.remove(".backup/head.prev")
-except:
-    pass
-try:
-    os.rename(".backup/head", ".backup/head.prev")
-except:
-    pass
-os.symlink("blobs/{}".format(digest), ".backup/head")
-
-print("head", ".backup/head")
-print("total_size", total_size)
-
+if __name__ == "__main__":
+    make_backup()
+    print("head", ".backup/head")
+    print("total_size", total_size)
 
